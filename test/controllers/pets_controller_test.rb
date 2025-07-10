5d0c29d6679da3741d488f034d1374334565932b
kaushik911 <kaushikshamantha@gmail.com>
2025-07-10 23:54:27 +0200

    * Complete requirements 5

--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -1,7 +1,51 @@
 require "test_helper"
 
 class PetsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @pet = Pet.create!(
+      pet_type: :cat,
+      tracker_type: :small,
+      owner_id: 1,
+      in_zone: false,
+      lost_tracker: false
+    )
+  end
+
+  test "should get index" do
+    get pets_url, as: :json
+    assert_response :success
+
+    body = JSON.parse(response.body)
+    assert body.is_a?(Array)
+    assert body.any? { |p| p["id"] == @pet.id }
+  end
+
+  test "should get outside_zone_count" do
+    get outside_zone_count_pets_url, as: :json
+    assert_response :success
+
+    body = JSON.parse(response.body)
+    assert body.any? { |item| item["pet_type"] == "cat" and item["tracker_type"] == "small"}
+  end
+
+  test "should not create invalid pet" do
+    pet_params = {
+      pet: {
+        pet_type: "cat",
+        tracker_type: "medium", # this gonna be only small and big
+        owner_id: 2,
+        in_zone: true,
+        lost_tracker: false
+      }
+    }
+
+    assert_difference 'Pet.count', 0 do
+      post pets_url, params: pet_params, as: :json
+      
+      assert_response :unprocessable_entity
+
+      body = JSON.parse(response.body)
+      assert_includes body["errors"].join, "cats cannot have medium tracker"
+    end
+  end
 end
--- test/models/pet_test.rb
+++ test/models/pet_test.rb
@@ -1,7 +1,65 @@
 require "test_helper"
 
 class PetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @pet = pets(:my_pet)
+  end
+
+  test "valid pet with correct attributes" do
+    assert @pet.valid?
+  end
+
+  test "invalid pet without pet_type" do
+    @pet.pet_type = nil
+    assert_not @pet.valid?
+    assert_includes @pet.errors[:pet_type], "can't be blank"
+  end
+
+  test "invalid pet without tracker_type" do
+    @pet.tracker_type = nil
+    assert_not @pet.valid?
+    assert_includes @pet.errors[:tracker_type], "can't be blank"
+  end
+
+  test "invalid pet without owner_id" do
+    @pet.owner_id = nil
+    assert_not @pet.valid?
+    assert_includes @pet.errors[:owner_id], "can't be blank"
+  end
+
+  test "pet in_zone must be true or false" do
+    @pet.in_zone = nil
+    assert_not @pet.valid?
+    assert_includes @pet.errors[:in_zone], "is not included in the list"
+  end
+
+  test "pet lost_tracker must be true or false" do
+    @pet.lost_tracker = nil
+    assert_not @pet.valid?
+    assert_includes @pet.errors[:lost_tracker], "is not included in the list"
+  end
+
+  test "cats cannot have medium tracker" do
+    @pet.tracker_type = :medium
+    assert_not @pet.valid?
+    assert_includes @pet.errors[:tracker_type], I18n.t("activerecord.errors.models.pet.attributes.tracker_type.cats_cannot_have_medium_tracker")
+  end
+
+  test "dogs can have medium tracker" do
+    @pet.pet_type = :dog
+    @pet.tracker_type = :medium
+    assert @pet.valid?
+  end
+
+  test "lost_tracker can only be true for cats" do
+    @pet.pet_type = :dog
+    @pet.lost_tracker = true
+    assert_not @pet.valid?
+    assert_includes @pet.errors[:lost_tracker], I18n.t("activerecord.errors.models.pet.attributes.lost_tracker.can_only_be_true_for_cats")
+  end
+
+  test "lost_tracker can be true for cats" do
+    @pet.lost_tracker = true
+    assert @pet.valid?
+  end
 end
